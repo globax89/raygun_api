368f2f8ef0fa1c66d91254a7592bb50f9283e190
Adwait Thattey <coderdude1999@gmail.com>
2020-04-04 08:22:37 +0530

    Setup Auth
    Setup Full Authentication including firebase oauth
    
    Signed-off-by: Adwait Thattey <coderdude1999@gmail.com>

--- raygun_api/urls.py
+++ raygun_api/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+from django.conf import settings
+from django.conf.urls.static import static
 
+schema_view = get_swagger_view(title="RayGun API")
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('docs', schema_view),
+    path('auth/', include("registration.urls")),
+    path('rest/', include('rest_framework.urls', namespace='rest_framework'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ registration/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from registration import views
+
+app_name="registration"
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', views.Hello.as_view(), name="hello"),
+    path('firebase/token', views.FirebaseTokenObtainPairView.as_view(), name="firebase_auth_token"),
+    path('firebase', views.FirebaseAuthenticationView.as_view(), name='firebase_auth'),
+]
